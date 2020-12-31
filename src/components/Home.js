5d1956320f3126948de4130fe9c38c40b1155bd4
JDBgit9 <info@jamesdbeeks.com>
2020-12-31 17:38:19 -0500

    updated home page syntax

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,12 +19,12 @@ function Home() {
     <a href="default.asp"><img src="./image/smirkhead.png" alt="my-headshot" style={{width: "100%"}} /></a>
     </div>
     <div className="col-sm-9">
-    <h5> My name is James, I am a Full-Stack Web Developer whose foundation is in the performing arts and I would like to welcome you to my digital portfolio. 
-        I have worked on projects that require HTML, CSS, JS, Node.js, mySQl, NoSQL, Dbeaver, Mongodb, express, REACTjs, and more. You can check out some examples of my work in the portfolio tab on the top right corner of this page. You will also find some links to the virtual demo of some applications that I created as well.
+    <h5> My name is James, I am a certified Full-Stack Web Developer who is a graduate of the University of Central Florida's Coding BootCamp program, and I welcome you to my digital portfolio. 
+        I have worked on projects that require HTML, CSS, JS, Node.js, mySQl, NoSQL, Dbeaver, Mongodb, Sequlize, heroku, express, REACTjs, and more. You can check out some examples of my work in the portfolio tab on the top right corner of this page. You will also find some links to the virtual demo of some applications that I created as well.
    </h5>
     <br />
     <h5>I grew up in Jacksonville, Florida and attended Douglas Anderson School Of the Arts, one of the nation's top Arts high schools to date. I am also an author with a new book titled "HisStory" to be released early 2021. I have been singing and performing on the Broadway stage and have always been very engaging with my audiences and will do the same in this new arena. 
-        I have always been in a creative mind space and want to bring my creativity to the developer industry. I took some web design and new media courses a few years ago and am building my portfolio and gaining more knowledge each day as a developer. I am excited to see where this journey will take me and I am honored to be a part of your team.    
+        I have always been in a creative mind space as my foundation is in the performing arts and want to bring my creativity to the developer industry. I took some web design and new media courses a few years ago and am building my portfolio and gaining more knowledge each day as a developer. I am excited to see where this journey will take me and I am honored to be a part of your team.    
     </h5>
     </div>
     </div>
