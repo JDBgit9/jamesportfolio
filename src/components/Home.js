2c45977fdc738bc41488a9856b001c7b7aa255a0
JDBgit9 <info@jamesdbeeks.com>
2020-12-31 17:51:55 -0500

    added certificate to the profile

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,11 +16,11 @@ function Home() {
     <div className="container-fluid">
     <div className="row">
     <div className="col-sm-3">
-    <a href="default.asp"><img src="./image/smirkhead.png" alt="my-headshot" style={{width: "100%"}} /></a>
+    <a href="./image/certificate"><img src="./image/smirkhead.png" style={{width: "100%"}}/><p>Click on image to view certificate</p></a>
     </div>
     <div className="col-sm-9">
     <h5> My name is James, I am a certified Full-Stack Web Developer who is a graduate of the University of Central Florida's Coding BootCamp program, and I welcome you to my digital portfolio. 
-        I have worked on projects that require HTML, CSS, JS, Node.js, mySQl, NoSQL, Dbeaver, Mongodb, Sequlize, heroku, express, REACTjs, and more. You can check out some examples of my work in the portfolio tab on the top right corner of this page. You will also find some links to the virtual demo of some applications that I created as well.
+        I have worked on projects that require HTML, CSS, JS, Node.js, mySQL, NoSQL, Dbeaver, Mongodb, Sequelize, Heroku, express, REACTjs, and more. You can check out some examples of my work in the portfolio tab on the top right corner of this page. You will also find some links to the virtual demo of some applications that I created as well.
    </h5>
     <br />
     <h5>I grew up in Jacksonville, Florida and attended Douglas Anderson School Of the Arts, one of the nation's top Arts high schools to date. I am also an author with a new book titled "HisStory" to be released early 2021. I have been singing and performing on the Broadway stage and have always been very engaging with my audiences and will do the same in this new arena. 
