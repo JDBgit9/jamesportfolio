5dc971b80ad8685ab6b1b866b8455ba93cda6554
JDBgit9 <info@jamesdbeeks.com>
2021-01-17 19:50:54 -0500

    updated app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Certificate from "./components/Certificate"
 
 
 function App() {
