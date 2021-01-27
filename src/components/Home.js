83297ed0c1a3a104fcea11cde3c61abf1a8ac5a4
JDBgit9 <info@jamesdbeeks.com>
2021-01-27 08:21:41 -0500

    chnaged syntax in the home

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,8 +20,8 @@ function Home() {
     <img src="./image/headshot2.png" style={{width: "100%"}} />
     </div>
     <div className="col-sm-9">
-    <p> My name is James, I am a certified Full-Stack Web Developer who is a graduate of the University of Central Florida's Coding BootCamp program, and I welcome you to my digital portfolio. 
-        I have worked on projects that require HTML, CSS, JS, Node.js, mySQl, NoSQL, Dbeaver, Mongodb, Sequelize, Heroku, express, REACTjs, and more. You can check out some examples of my work in the portfolio tab on the top right corner of this page. You will also find some links to the virtual demo of some applications that I created as well.
+    <p> Welcome you to my digital portfolio. My name is James and I am a certified Full-Stack Web Developer who is a graduate of the University of Central Florida's Coding BootCamp program. 
+        I have worked on projects that require HTML, CSS, JS, Node.js, mySQl, NoSQL, Dbeaver, Mongodb, Sequelize, Heroku, express, REACTjs, and more. You can view some examples of my work in the portfolio tab on the top right corner of this page. You will also find some links to the virtual demo of some applications that I created as well.
    </p>
     <br />
     <p>I grew up in Jacksonville, Florida and attended Douglas Anderson School Of the Arts, one of the nation's top Arts high schools to date. I am also an author with a new book titled "HisStory" to be released early 2021. I have been singing and performing on the Broadway stage and have always been very engaging with my audiences and will do the same in this new arena. 
