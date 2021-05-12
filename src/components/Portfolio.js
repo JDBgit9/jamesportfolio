ebd36880c2f433f513b09c3e0ded537d660d640b
JDBgit9 <info@jamesdbeeks.com>
2021-05-12 18:17:54 -0400

    added links and phots, changed syntax

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,14 +5,15 @@ function Portfolio() {
   const dir = "./image";
   return (
     <div>
-      <h2>Portfolio</h2>
+      <h2>My Portfolio</h2>
       <br />
+      <p>These are projects I have created and/or have worked on. Click on images to take you to the application. Have fun but dont forget to come back.</p>
+      
+      <a href="https://memebate.herokuapp.com/">
+        <img src="./image/memebate.png" style={{ width: "45%" }} />
+      </a>
       <a href="https://quiet-earth-28574.herokuapp.com/login">
       <img src="./image/memberspage.png" style={{ width: "45%" }} />
-      </a>
-    
-      <a href="https://memebate.herokuapp.com/">
-        <img src="./image/memebate.png" style={{ width: "47%" }} />
       </a>
       <a href="https://jdbgit9.github.io/Weather-App/">
         <img src="./image/weatherapp2.png" style={{ width: "45%" }} />
