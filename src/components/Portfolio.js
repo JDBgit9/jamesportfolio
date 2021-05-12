c217e27143843aed5962584b6d3d5c82837a1ec9
JDBgit9 <info@jamesdbeeks.com>
2021-05-12 17:47:43 -0400

    changing the portfolio page by adding photos and links toall of my work simplify the page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,50 +6,47 @@ function Portfolio() {
   return (
     <div>
       <h2>Portfolio</h2>
-      <a href={`${dir}/TechRes221.png`}>
-        <img src="./image/nerdlook.png" style={{ width: "20%" }} />
-        <p>Click on image for Resume</p>
-      </a>
       <br />
-      <img src="./image/memberspage.png" style={{ width: "40%" }} />
-      <a href="https://quiet-earth-28574.herokuapp.com/">
-        <img src="./image/artbook.png" style={{ width: "40%" }} />
+      <a href="https://quiet-earth-28574.herokuapp.com/login">
+      <img src="./image/memberspage.png" style={{ width: "45%" }} />
+      </a>
+    
+      <a href="https://memebate.herokuapp.com/">
+        <img src="./image/memebate.png" style={{ width: "47%" }} />
       </a>
      
       <a href="https://aqueous-taiga-71567.herokuapp.com/">
-        <img src="./image/brrger.png" style={{ width: "40%" }} />
-        <p>Click to play The Burger Game</p>
+        <img src="./image/brrger.png" style={{ width: "45%" }} />
       </a>
-      <br />
       <a href="https://jdbgit9.github.io/Weather-App/">
-        <img src="./image/weatherapp2.pn" style={{ width: "40%" }} />
-        <p />
-      </a>
-       
+        <img src="./image/weatherapp2.png" style={{ width: "45%" }} />
+      </a>  
       <a href="https://damp-journey-73235.herokuapp.com/">
-        <img src="./image/fitnesstracker.png" style={{ width: " 40" }} />
-        Hello
+        <img src="./image/fitnesstracker.png" style={{ width: "45" }} />
       </a>
       <br />
-      <img src="./image/PasswordGen.png" style={{ width: "50%" }} />
+      <img src="./image/PasswordGen.png" style={{ width: "45%" }} />
       <br />
-      <img src="./image/codequiz2.png" style={{ width: "50%" }} />
+      <img src="./image/codequiz2.png" style={{ width: "45%" }} />
       <br /> 
-      <img src="./image/moviedine2.png" style={{ width: "50%" }} />
-      <img src="./image/md8.png" style={{ width: "50%" }} />
-      <img src="./image/md10.png" style={{ width: "50%" }} />
+      <img src="./image/moviedine2.png" style={{ width: "45%" }} />
+      <img src="./image/md8.png" style={{ width: "45%" }} />
+      <img src="./image/md10.png" style={{ width: "45%" }} />
        <br />
       
       <div class="column" />
     
       <a />
-      <img src="./image/sattelite1.jpeg" style={{ width: "50%" }} />
+      <img src="./image/sattelite1.jpeg" style={{ width: "45%" }} />
       <a />
-      <img src="./image/sattelite2.jpeg" style={{ width: "50%" }} />
+      <img src="./image/sattelite2.jpeg" style={{ width: "45%" }} />
       <a />
       <a href="https://github.com/JDBgit9">
         <img src="./image/github logo1.jpeg" style={{ width: "20%" }} />
-        <img src="./image/JCSBWW.jpeg" style={{ width: "50%" }} />
+      </a>
+      <a href={`${dir}/TechRes221.png`}>
+        <img src="./image/nerdlook.png" style={{ width: "45%" }} />
+        <p>Click on image for Resume</p>
       </a>
     </div>
   );
