be19eca14af23cca3fa54c5f592abda71fa868d0
JDBgit9 <info@jamesdbeeks.com>
2021-05-14 11:35:29 -0400

    changed the syntax size in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
              <nav className="navbar navbar-expand-lg navbar-light bg-grey">
         <div className="container">
             <a href="/" className="navbar-brand header">
-                <h1>James Beeks</h1></a>
+                <h2>James Beeks</h2></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -14,8 +14,8 @@ function Header() {
             <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
                     <a className="nav-link active" href="/"><span className="sr-only">(current)</span></a>
-                    <a className="nav-link" href="/portfolio">Portfolio</a>
-                    <a className="nav-link" href="/contact">Contact</a>
+                    <a className="nav-link" href="/portfolio"><h2>Portfolio</h2></a>
+                    <a className="nav-link" href="/contact"><h2>Contact</h2></a>
                 </div>
             </div>
         </div>
