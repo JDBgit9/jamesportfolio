8664c31e7630aef731a0d20560ac6b36ff8be9e7
JDBgit9 <jamesthejust99@outlook.com>
2022-04-19 09:50:17 -0400

    Added a h5 tag and syntax to portjs line 28

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -25,6 +25,7 @@ function Portfolio() {
       <a href="https://jdbgit9.github.io/Workout-Tracker/">
       <img src="./image/fitnesstracker.png" style={{ width: "60%" }} />
       </a> 
+      <h5>MemeBate</h5>
       <a href="https://memebate.herokuapp.com/">
         <img src="./image/memebate.png" style={{ width: "60%" }} />
       </a>
