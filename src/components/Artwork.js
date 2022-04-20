a68549fd3e06e07f5d269e199665efa160f395bb
JDBgit9 <jamesthejust99@outlook.com>
2022-04-20 16:57:34 -0400

    Made changes to the artworkjs

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -2,13 +2,15 @@ import React from 'react';
 import "./Artwork.css"; 
 
 function Artwork() {
+  const dir = "./image";
     return (
-        <div className="column">
-          <h2>Here is some of my artwork</h2>
+        <div>
+          <h3>Here is some of my artwork</h3>
           <img src="./image/sattelite1.jpeg" style={{ width: "60%" }} />
           <img src="./image/sattelite2.jpeg" style={{ width: "60%" }} />
+        <img src="./image/" />
         </div>
-    )
+    );
 }
 
 export default Artwork;
