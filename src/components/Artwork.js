558cba45d3690c5880db821848c4a6aae80e6c3c
JDBgit9 <jamesthejust99@outlook.com>
2022-04-20 16:41:08 -0400

    added img tag to artworkjs

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -3,8 +3,10 @@ import "./Artwork.css";
 
 function Artwork() {
     return (
-        <div className="col-lg-12-center">
+        <div className="column">
+          <h2>Here is some of my artwork</h2>
           <img src="./image/sattelite1.jpeg" style={{ width: "60%" }} />
+          <img src="./image/sattelite2.jpeg" style={{ width: "60%" }} />
         </div>
     )
 }
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -51,7 +51,7 @@ function Portfolio() {
       <div class="column" />
      <a className="nav-link" href="/artwork"><p>Here is some of my artwork</p></a>
      
-      <img src="./image/sattelite2.jpeg" style={{ width: "60%" }} />
+    
       <a />
       <img src="./image/nerdlook.png" style={{ width: "30%" }} />
       <a href="https://github.com/JDBgit9">
