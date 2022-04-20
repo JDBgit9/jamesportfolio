347e6de6ba9761f731dfc9338a092d6302eeeb3c
JDBgit9 <jamesthejust99@outlook.com>
2022-04-20 19:12:49 -0400

    cchanged the syntax to the correct site portjs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -38,8 +38,8 @@ function Portfolio() {
       <a href="https://memebate.herokuapp.com/">
         <img src="./image/memebate.png" style={{ width: "60%" }} />
       </a>
-      <h5>ArtBook</h5>
-      <a href="https://quiet-earth-28574.herokuapp.com/login">
+      <h5>ArtRoom</h5>
+      <a href="https://theartroom.herokuapp.com/">
       <img src="./image/memberspage.png" style={{ width: "60%" }} />
       </a>
       <h5>Note Taker</h5>
