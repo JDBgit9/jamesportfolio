94948744a9033ac53d46f2727d0df7b341677164
JDBgit9 <jamesthejust99@outlook.com>
2022-04-20 16:17:16 -0400

    changed h3 tag to h4 in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ function Header() {
                 <div className="navbar-nav">
                     <a className="nav-link active" href="/"><span className="sr-only">(current)</span></a>
                     <a className="nav-link" href="/portfolio"><h4>Portfolio</h4></a>
-                    <a className="nav-link" href="/artwork"><h3>Artwork</h3></a>
+                    <a className="nav-link" href="/artwork"><h4>Artwork</h4></a>
                     <a className="nav-link" href="/contact"><h3>Contact</h3></a>
                 </div>
             </div>
