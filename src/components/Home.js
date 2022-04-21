ec3556ec206f33ec617cedfa1fedc04f4d8698d1
JDBgit9 <jamesthejust99@outlook.com>
2022-04-21 17:53:13 -0400

    commented out the badge for now

--- src/components/Home.js
+++ src/components/Home.js
@@ -25,9 +25,9 @@ function Home() {
         You can view examples of my work in the portfolio tab on the top right corner of this page. I am excited to see where this journey will take me and I am honored to be a part of your team.
     </p>
     <br />
-     <div className="col-sm-4-left">
+     {/*<div className="col-sm-4-left">
      <img src="./image/digitalbadge.png" style={{width: "10%"}} class="left" />
-    </div>
+    </div>*/}
     </div>
     </div>
     </div>
