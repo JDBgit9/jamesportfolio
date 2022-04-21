2dd34ce70ac3ff9f49eb303b9eb0fbfd3e77024c
JDBgit9 <jamesthejust99@outlook.com>
2022-04-21 16:55:25 -0400

    added syntax to the end p tag in homejs

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,17 +22,13 @@ function Home() {
     <div className="col-sm-9">
     <p> My name is James and I am a certified Full-Stack Web Developer who is a graduate of the University of Central Florida with a certificate degree in Coding. 
         I have worked on projects that require extensive knowledge of HTML, CSS, JS, Node.js, mySQl, NoSQL, Dbeaver, Mongodb, Sequelize, Yarn, Heroku, express, REACTjs, and more.
-        You can view examples of my work in the portfolio tab on the top right corner of this page. 
+        You can view examples of my work in the portfolio tab on the top right corner of this page. I am excited to see where this journey will take me and I am honored to be a part of your team.
     </p>
     <br />
-    <
-    <div className="col-sm-4-center">p> 
-        I am a problem solver, a fast learner, and very professional with strong wit, 
-        so I am a blast to be around. I have always been in a creative mind space as my 
-        foundation is in the performing arts and want to bring my creativity to the developer industry. 
-        I took some web design and new media courses a few years ago and am building my portfolio and 
-        gaining more knowledge each day as a developer.     
+    <p> 
+        I am a problem solver, a fast learner, and very professional with strong wit, so I am a blast to be around. I have always been in a creative mind space as my foundation is in the performing arts and want to bring my creativity to the developer industry. I took some web design and new media courses a few years ago and am building my portfolio and gaining more knowledge each day as a developer.     
     </p>
+    <div className="col-sm-4-center">
     <img src="./image/digitalbadge.png" style={{width: "20%"}} class="center" />
     </div>
     </div>
