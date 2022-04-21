c3274225c2d090727a8ca51dfda0f8c4ee738e47
JDBgit9 <jamesthejust99@outlook.com>
2022-04-21 17:06:44 -0400

    updated sytax in artworkjs

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -8,7 +8,7 @@ function Artwork() {
             <p> 
         I am a problem solver, a fast learner, and very professional with strong wit, so I am a blast to be around. I have always been in a creative mind space as my foundation is in the performing arts and want to bring my creativity to the developer industry. I took some web design and new media courses a few years ago and am building my portfolio and gaining more knowledge each day as a developer.     
     </p>
-          <h3>Here is some of my artwork</h3>
+          <h3>Here is some of my hand drawn sketches</h3>
           <img src="./image/memebate.jpg" style={{width: "60%"}} />
           <img src="./image/memebatedraft.jpg" style={{width: "60%"}} />
           <img src="./image/sattelite1.jpeg" style={{width: "60%" }} />
