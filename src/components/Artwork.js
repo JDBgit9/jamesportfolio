345d0cc592ca69a705c5f8c68591e97684e5ccf1
JDBgit9 <jamesthejust99@outlook.com>
2022-04-23 15:30:35 -0400

    addeda new image to artworkjs

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -9,7 +9,8 @@ function Artwork() {
         I am a problem solver, a fast learner, and very professional with strong wit, so I am a blast to be around. I have always been in a creative mind space as my foundation is in the performing arts and want to bring my creativity to the developer industry. I took some web design and new media courses a few years ago and am building my portfolio and gaining more knowledge each day as a developer.     
     </p>
           <h3>Here is some of my hand drawn sketches</h3>
-          <img src="./image/b" style={{width: "60%"}} />
+          <img src="./image/ballsketch.jpg" style={{width: "60%"}} />
+          <img src="./image/bottlesketch.jpg" style={{width: "60%"}} />
           <img src="./image/memebate.jpg" style={{width: "60%"}} />
           <img src="./image/memebatedraft.jpg" style={{width: "60%"}} />
           <img src="./image/sattelite1.jpeg" style={{width: "60%" }} />
