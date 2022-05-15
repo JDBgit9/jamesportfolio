7e753d95cdf5f93e6b2d140889877cb93bc45790
JDBgit9 <jamesthejust99@outlook.com>
2022-05-15 10:24:39 -0400

    added alt and value to images portjs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,45 +10,45 @@ function Portfolio() {
       <p>Scroll down and click on the images to take you the application for your perusal. Enjoy!</p>
        <hr />
       <h5>The Weather App</h5><p>Check the weather in your area...HTML/CSS/JS</p> 
-      <a href="https://jdbgit9.github.io/Weather-App/" alt="Weather API site">
-        <img src="./image/weatherapp2.png" style={{ width: "60%" }} />  
+      <a href="https://jdbgit9.github.io/Weather-App/">
+        <img src="./image/weatherapp2.png" alt="weather API page image" style={{ width: "60%" }} />  
         </a>  
         <h5>Password Generator</h5>
         <a href="https://jdbgit9.github.io/Password-Generator/">
-          <img src="./image/passwordgen.png" style={{width: "60%" }}/>
+          <img src="./image/passwordgen.png" alt="password generator image" style={{width: "60%" }}/>
           </a>
           <h5>Code Quiz</h5>
           <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
-            <img src="./image/codequiz.png" style={{width: "60%"}}/>
+            <img src="./image/codequiz.png" alt="codequiz image" style={{width: "60%"}}/>
           </a>
           <h5>ArtRoom</h5>
       <a href="https://theartroom.herokuapp.com/">
-      <img src="./image/memberspage.png" style={{ width: "60%" }} />
+      <img src="./image/memberspage.png"  alt="members page entrance" style={{ width: "60%" }} />
       </a>
 
         <h5>BurgerTime</h5>
         <a href="https://aqueous-taiga-71567.herokuapp.com/">
-        <img src="./image/brrger.png" style={{ width: "60%" }} />
+        <img src="./image/brrger.png" alt="burger image" style={{ width: "60%" }} />
       </a>
       <h5>Fitness Tracker</h5>
       <a href="https://jdbgit9.github.io/Workout-Tracker/">
-      <img src="./image/fitnesstracker.png" style={{ width: "60%" }} />
+      <img src="./image/fitnesstracker.png" alt="fitnesstracker image" style={{ width: "60%" }} />
       </a> 
       <h5>StreamGrub</h5> 
       <a href="https://jdbgit9.github.io/StreamGrub/">
-        <img src="./image/moviedine2.png" style={{ width: "60%" }} /></a>
+        <img src="./image/moviedine2.png" alt="StreamGrug image" style={{ width: "60%" }} /></a>
       <h5>Note Taker</h5>
       <a href="https://jdbgit9.github.io/Note-Taker/">
-      <img src="./image/notetaker.png" style={{ width: "60%" }} />
+      <img src="./image/notetaker.png" alt="note taker image" style={{ width: "60%" }} />
       </a>
        <br />
        <h5>MemeBate</h5>
       <a href="https://memebate.herokuapp.com/">
-        <img src="./image/memebate.png" style={{ width: "60%" }} />
+        <img src="./image/memebate.png" alt="MemeBate image" style={{ width: "60%" }} />
       </a>
       <br />
        <br /> 
-      <img src="./image/nerdlook.png" style={{ width: "30%" }} />
+      <img src="./image/nerdlook.png" alt="coder image" style={{ width: "30%" }} />
       <a href="https://github.com/JDBgit9">
         <br />
         <br />
