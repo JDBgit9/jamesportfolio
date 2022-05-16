4fa38022578d53750a4a39e2f75900c8a69f5d8d
JDBgit9 <jamesthejust99@outlook.com>
2022-05-16 16:47:32 -0400

    changed classname of ptag to intro fro css

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -5,7 +5,7 @@ function Artwork() {
 
     return (
         <div>
-            <p> 
+            <p className="intro"> 
         I am a problem solver, a fast learner, and very professional with strong wit, so I am a blast to be around. I have always been in a creative mind space as my foundation is in the performing arts and want to bring my creativity to the developer industry. I took some web design and new media courses a few years ago and am building my portfolio and gaining more knowledge each day as a developer.     
     </p>
     <br />
