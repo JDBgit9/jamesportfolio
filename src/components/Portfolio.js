0320a67bb2b3f668131d3a48096e0ded269eb541
JDBgit9 <jamesthejust99@outlook.com>
2022-05-18 16:02:00 -0400

    moved notetaker upon portjs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,8 +9,8 @@ function Portfolio() {
       <br />
       <p>Scroll down and click on the images to take you the application for your perusal. Enjoy!</p>
        <hr />
-      <h5>The Weather App</h5><p>Check the weather in your area...HTML/CSS/JS</p> 
-      <a href="https://jdbgit9.github.io/Weather-App/">
+        <h5>The Weather App</h5><p>Check the weather in your area...HTML/CSS/JS</p> 
+        <a href="https://jdbgit9.github.io/Weather-App/">
         <img src="./image/weatherapp2.png" alt="weather API page" style={{ width: "60%" }} />  
         </a>  
         <h5>Password Generator</h5>
@@ -21,10 +21,14 @@ function Portfolio() {
           <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
             <img src="./image/codequiz.png" alt="codequiz" style={{width: "60%"}}/>
           </a>
+          <h5>Note Taker</h5>
+          <a href="https://notes-9.herokuapp.com/">
+           <img src="./image/notetaker.png" alt="note taker" style={{ width: "60%" }} />
+          </a>
           <h5>ArtRoom</h5>
-      <a href="https://theartroom.herokuapp.com/">
-      <img src="./image/memberspage.png"  alt="members page entrance" style={{ width: "60%" }} />
-      </a>
+          <a href="https://theartroom.herokuapp.com/">
+         <img src="./image/memberspage.png"  alt="members page entrance" style={{ width: "60%" }} />
+         </a>
 
         <h5>BurgerTime</h5>
         <a href="https://aqueous-taiga-71567.herokuapp.com/">
@@ -37,10 +41,6 @@ function Portfolio() {
       <h5>StreamGrub</h5> 
       <a href="https://jdbgit9.github.io/StreamGrub/">
         <img src="./image/moviedine2.png" alt="StreamGrug" style={{ width: "60%" }} /></a>
-      <h5>Note Taker</h5>
-      <a href="https://notes-9.herokuapp.com/">
-      <img src="./image/notetaker.png" alt="note taker" style={{ width: "60%" }} />
-      </a>
        <br />
        <h5>MemeBate</h5>
       <a href="https://memebate.herokuapp.com/">
