a37c779b2ad94cbec6bc49d3704c35f2a7c6d004
JDBgit9 <jamesthejust99@outlook.com>
2022-06-09 13:15:07 -0400

    Changed quotations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,49 @@
-import React from 'react';
+import React from "react";
 import "./Home.css";
 
-
 function Home() {
-    return (
-        <div>
-              <h4>About Me</h4>
-    <div className="wrapper">
+  return (
+    <div>
+      <h4>About Me</h4>
+      <div className="wrapper">
         <div className="container-mt-4">
-            <div className="row">
-                <div className="col-md-9-bg-white">
-                </div>
-            </div>
+          <div className="row">
+            <div className="col-md-9-bg-white"></div>
+          </div>
         </div>
-    </div>
-    <div className="container-fluid">
-    <div className="row">
-    <div className="col-sm-3">
-    <img src="./image/portfolioicon.jpg" style={{width: "100%"}} className="mainpic" alt="coder" />
-    </div>
-    <div className="col-sm-9">
-    <p> Hi, I'm James and I am a certified Full-Stack Web Developer who is a graduate of the University of Central Florida with a certificate degree in Coding. 
-        I have worked on projects that require extensive knowledge of HTML, CSS, JS, Node.js, mySQl, NoSQL, Dbeaver, Mongodb, Sequelize, Yarn, Heroku, express, REACTjs, and more.
-        You can view examples of my work in the portfolio tab on the top right corner of this page. I am excited to see where this journey will take me and I am honored to be a part of your team.
-    </p>
-    <br />
-      {/* haven't decided if I want this on the home page or not.  it is on the portfolio and soon to be on the contact page. */}
-     {/* <div className="col-sm-4-left"> */}
-     {/* <img src="./image/digitalbadge.png" style={{width: "10%"}} class="left" /> */}
-    {/* </div> */}
-    </div>
-    </div>
-    </div>
+      </div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-3">
+            <img
+              src="./image/portfolioicon.jpg"
+              style={{ width: "100%" }}
+              className="mainpic"
+              alt="coder"
+            />
+          </div>
+          <div className="col-sm-9">
+            <p>
+              {" "}
+              Hi, I'm James and I am a certified Full-Stack Web Developer who is
+              a graduate of the University of Central Florida with a certificate
+              degree in Coding. I have worked on projects that require extensive
+              knowledge of HTML, CSS, JS, Bootstrap, Node.js, mySQl, NoSQL,
+              Dbeaver, Mongodb, Sequelize, Yarn, Heroku, express, REACTjs, and
+              more. You can view examples of my work in the portfolio tab on the
+              top right corner of this page. I am excited to see where this
+              journey will take me and I am honored to be a part of your team.
+            </p>
+            <br />
+            {/* haven't decided if I want this on the home page or not.  it is on the portfolio and soon to be on the contact page. */}
+            {/* <div className="col-sm-4-left"> */}
+            {/* <img src="./image/digitalbadge.png" style={{width: "10%"}} class="left" /> */}
+            {/* </div> */}
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
