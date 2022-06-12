d598e9f906fb1beeb8f3405bb731a818f36d67d5
JDBgit9 <jamesthejust99@outlook.com>
2022-06-12 19:58:41 -0400

    added contaner div to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,10 @@ import "./Footer.css";
 
 function Footer() {
     return (
-
-        <div className="footer">
+       <div className='Container'>
+        <div className="footer" style={{width: "100%"}}>
             <p className='foot-text'>James Justis | GitHub: JDBgit9 | email: justiscode@outlook.com. &copy; 2022</p>
+        </div>
         </div>
     )
 }
