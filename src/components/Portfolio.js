0ec22436e9426e958cb6130dabd2d461ce23727a
JDBgit9 <jamesthejust99@outlook.com>
2022-06-14 20:53:46 -0400

    resizing imgs in portjs/css

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,98 +5,99 @@ function Portfolio() {
   const dir = "./image";
   return (
     <div>
-      <h3>My Portfolio</h3>
+      <h2>My Portfolio</h2>
       <br />
     
       <hr />
-      <h5>TinDog</h5>
+
       <a href="https://jdbgit9.github.io/Tin-Dogger/">
         <img
           src="./image/tindog.png"
           alt="tinder for dog app"
-          style={{ width: "60%" }}
+          style={{ width: "30%" }}
         />
       </a>
-      <h5>The Weather App</h5>
-      <p>Check the weather in your area...HTML/CSS/JS</p>
+      <b />
+  
+      <p></p>
       <a href="https://jdbgit9.github.io/Weather-App/">
         <img
           src="./image/weatherapp2.png"
           alt="weather API page"
-          style={{ width: "60%" }}
+          style={{ width: "30%" }}
         />
       </a>
-      <h5>Password Generator</h5>
+      <br />
+      <br />
       <a href="https://jdbgit9.github.io/Password-Generator/">
         <img
           src="./image/passwordgen.png"
           alt="password generator"
-          style={{ width: "60%" }}
+          style={{ width: "30%" }}
         />
       </a>
-      <h5>Code Quiz</h5>
+     <br />
       <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
         <img
           src="./image/codequiz.png"
           alt="codequiz"
-          style={{ width: "60%" }}
+          style={{ width: "30%" }}
         />
       </a>
-      <h5>Note Taker</h5>
+      <br />
       <a href="https://notes-9.herokuapp.com/">
         <img
           src="./image/notetaker.png"
           alt="note taker"
-          style={{ width: "60%" }}
+          style={{ width: "30%" }}
         />
       </a>
-      <h5>ArtRoom</h5>
+   
       <a href="https://theartroom.herokuapp.com/">
         <img
           src="./image/memberspage.png"
           alt="members page entrance"
-          style={{ width: "60%" }}
+          style={{ width: "30%" }}
         />
       </a>
-      <h5>BurgerTime</h5>
+      <br />
       <a href="https://aqueous-taiga-71567.herokuapp.com/">
-        <img src="./image/brrger.png" alt="burger" style={{ width: "60%" }} />
+        <img src="./image/brrger.png" alt="burger" style={{ width: "30%" }} />
       </a>
-      <br /> <br />
-      <h5>Fitness Tracker</h5>
+      <br /> 
+      <br />
+     
       <a href="https://jdbgit9.github.io/Workout-Tracker/">
         <img
           src="./image/fitnesstracker.png"
           alt="fitnesstracker"
-          style={{ width: "60%" }}
+          style={{ width: "25%" }}
         />
       </a>
       <br />
       <br />
-      <h5>StreamGrub</h5>
+    
       <a href="https://jdbgit9.github.io/StreamGrub/">
         <img
           src="./image/moviedine2.png"
           alt="StreamGrug"
-          style={{ width: "60%" }}
+          style={{ width: "25%" }}
         />
       </a>
       <br />
       <br />
-      <h5>MemeBate</h5>
+    
       <a href="https://memebate.herokuapp.com/">
         <img
           src="./image/memebate.png"
           alt="MemeBate"
-          style={{ width: "60%" }}
+          style={{ width: "25%" }}
         />
       </a>
       <br />
+    
       <br />
-      <img src={`${dir}/techres22.png`} alt="Resume" style={{ width: "60%" }} />
-      <br />
-      <br />
-      <img src="./image/nerdlook.png" alt="headshot" style={{ width: "30%" }} />
+      <img src="./image/nerdlook.png" alt="headshot" style={{ width: "25%" }} />
       <a href="https://github.com/JDBgit9">
         <br />
         <br />
