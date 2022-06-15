c539047e28966c50d83e40b8a635ef2cbc7e6ba8
JDBgit9 <jamesthejust99@outlook.com>
2022-06-14 20:07:55 -0400

    more syntax correction home js

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,8 +31,7 @@ function Home() {
                 {" "}
                 Hi, I'm James and I am a certified Full-Stack Web Developer who
                 is a graduate of the University of Central Florida with a
-                certificate degree in Coding. I have worked on projects that
-                require extensive knowledge of HTML, CSS, JS, Bootstrap,
+                certificate degree in Coding. I have extensive knowledge of HTML, CSS, JavaScript, Bootstrap,
                 Node.js, mySQl, NoSQL, Dbeaver, Mongodb, Sequelize, Yarn,
                 Heroku, express, REACTjs, and more.
                 I am excited to see where this journey will take me and I am
