d8a023cf1a6f9472221d1dca222b22a5d290cd0d
JDBgit9 <jamesthejust99@outlook.com>
2022-06-16 07:03:19 -0400

    added image to artwork js resized

--- src/components/Artwork.js
+++ src/components/Artwork.js
@@ -1,70 +1,138 @@
-import React from 'react';
-import "./Artwork.css"; 
+import React from "react";
+import "./Artwork.css";
 
 function Artwork() {
+  return (
+    <div>
+      <p className="intro">
+        I am a problem solver, a fast learner, and very professional with strong
+        wit, so I am a blast to be around. I have always been in a creative mind
+        space as my foundation is in the performing arts and want to bring my
+        creativity to the developer industry. As an web design and new media and
+        am building my portfolio and gaining more knowledge each day as a
+        developer.
+      </p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <hr className="lineBreak" />
 
-    return (
-        <div>
-            <p className="intro"> 
-        I am a problem solver, a fast learner, and very professional with strong wit, so I am a blast to be around. I have always been in a creative mind space as my foundation is in the performing arts and want to bring my creativity to the developer industry. I took some web design and new media courses a few years ago and am building my portfolio and gaining more knowledge each day as a developer.     
-    </p>
-    <br />
-          <br />
-          <h3>Here is some of my hand drawn sketches</h3>
+      <div className="row">
+        <div className="col-lg-4">
+          <img src="./image/josh.jpg" alt="Josh" style={{ width: "100%" }} />
           <br />
           <br />
-          <hr className="lineBreak" />
-          <img src="./image/josh.jpg" alt="Josh" style={{width: "40%"}} />
+          <img
+            src="./image/facialemotes.jpg"
+            alt="facial emotions"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/facialemotes.jpg" alt="facial emotions" style={{width: "40%"}} />
+          <img
+            src="./image/ballsketch.jpg"
+            alt="Ball"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/ballsketch.jpg" alt="Ball" style={{width: "40%"}} />
+          <img
+            src="./image/bottlesketch.jpg"
+            alt="Bottle"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/bottlesketch.jpg" alt="Bottle" style={{width: "40%"}} />
+          <img
+            src="./image/virtualpark.jpg"
+            alt="theme park map"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/virtualpark.jpg" alt="theme park map" style={{width: "40%"}} />
+          <img
+            src="./image/virtualentry.jpg"
+            alt="theme park city view"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/virtualentry.jpg" alt="theme park city view" style={{width: "40%"}} />
+          <img
+            src="./image/memebate.jpg"
+            alt="Sattelight"
+            style={{ width: "100%" }}
+          />
+
           <br />
           <br />
-          <img src="./image/memebate.jpg" alt="Sattelight" style={{width: "40%"}} />
-          
+          <img
+            src="./image/memebatedraft.jpg"
+            alt="MemeBate wireframe"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/memebatedraft.jpg" alt="MemeBate wireframe" style={{width: "60%"}} />
+          <img
+            src="./image/sattelite1.jpeg"
+            alt="Sattelight"
+            style={{ width: "100%" }}
+          />
+
           <br />
           <br />
-          <img src="./image/sattelite1.jpeg" alt="Sattelight" style={{width: "40%" }} />
-          <p>A new superhero Character I am developing named Sattelight (Working title)</p>
+          <img
+            src="./image/sattelite2.jpeg"
+            alt="Sattelight"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/sattelite2.jpeg" alt="Sattelight" style={{width: "40%" }} />
+          <img
+            src="./image/satteliteschema.jpg"
+            alt="Sattelight"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/satteliteschema.jpg" alt="Sattelight" style={{width: "40%" }} />
+       
+          <img
+            src="./image/justiscity.jpg"
+            alt="Justis City Map"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <p>I like to make up my own maps of new cities, this is Justis City</p>
-          <img src="./image/justiscity.jpg" alt="Justis City Map" style={{width: "40%"}} />
+          <img
+            src="./image/justiscityview.jpg"
+            alt="justiscity Bay"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src='./image/justiscityview.jpg' alt='justiscity Bay' style={{width: "40%"}} />
+          <img
+            src="./image/sslegendpf2.jpg"
+            alt="SSLegend Space craft"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/sslegendpf2.jpg" alt="SSLegend Space craft" style={{width: "40%"}} />
+          <img
+            src="./image/shiplegend.jpg"
+            alt="SSlegend Design"
+            style={{ width: "100%" }}
+          />
           <br />
           <br />
-          <img src="./image/shiplegend.jpg" alt="SSlegend Design" style={{width: "40%"}} />
-          <br />
-          <br />
-          <img src="./image/sslegendpf1.jpg" alt="SSLegend sketch" style={{width: "40%"}} />
+          <img
+            src="./image/sslegendpf1.jpg"
+            alt="SSLegend sketch"
+            style={{ width: "100%" }}
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Artwork;
