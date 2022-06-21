72e7f722881ac35c51fb0c69dfead47160f95da2
JDBgit9 <jamesthejust99@outlook.com>
2022-06-20 22:47:34 -0400

    added divs and rows and reposition in portjs

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,17 +4,16 @@ import "./Portfolio.css";
 function Portfolio() {
   const dir = "./image";
   return (
-    <div>
+    <div className="row"> 
+    <div className="col-md-6">
       <h2 className="port-head">My Portfolio</h2>
       <br />
     
-
-
       <a href="https://jdbgit9.github.io/Tin-Dogger/">
         <img
           src="./image/tindog.png"
           alt="tinder for dog app"
-          style={{ width: "55%" }}
+          style={{ width: "90%" }}
         />
       </a>
       <br />
@@ -29,17 +28,21 @@ function Portfolio() {
         <img
           src="./image/weatherapp2.png"
           alt="weather API page"
-          style={{ width: "55%" }}
+          style={{ width: "65%" }}
         />
       </a>
       <br />
       <br />
-
+  
+    </div>
+      
+    <div className="col-md-6 two-col">
+       
       <a href="https://6278331b098bd11537dbb012--countdown-timer-solutions.netlify.app/">
         <img
           src="./image/countdown.png"
           alt="countdown API page"
-          style={{ width: "55%" }}
+          style={{ width: "65%" }}
         />
       </a>
       <br />
@@ -49,7 +52,7 @@ function Portfolio() {
         <img
           src="./image/passwordgen.png"
           alt="password generator"
-          style={{ width: "55%" }}
+          style={{ width: "85%" }}
         />
       </a>
      <br />
@@ -58,7 +61,7 @@ function Portfolio() {
         <img
           src="./image/codequiz.png"
           alt="codequiz"
-          style={{ width: "55%" }}
+          style={{ width: "65%" }}
         />
       </a>
       <br />
@@ -67,24 +70,26 @@ function Portfolio() {
         <img
           src="./image/notetaker.png"
           alt="note taker"
-          style={{ width: "55%" }}
+          style={{ width: "65%" }}
         />
       </a>
       <br />
       <br />
 
+      <div className="col-md-6">
+
       <a href="https://work-0ut.herokuapp.com/">
         <img
           src="./image/fitnesstracker.png"
           alt="fitnesstracker"
-          style={{ width: "55%" }}
+          style={{ width: "75%" }}
         />
       </a>
       <br />
       <br />
 
       <a href="https://shimmering-sopapillas-3df067.netlify.app/">
-        <img src="./image/shortly.png" alt="landing page" style={{ width: "55%" }} />
+        <img src="./image/shortly.png" alt="landing page" style={{ width: "25%" }} />
       </a>
       <br />
       <br />
@@ -93,22 +98,22 @@ function Portfolio() {
         <img
           src="./image/moviedine2.png"
           alt="StreamGrug"
-          style={{ width: "55%" }}
+          style={{ width: "25%" }}
         />
       </a>
       <br />
       <br />
-
+    </div>
       <a href="https://theartroom.herokuapp.com/">
         <img
           src="./image/memberspage.png"
           alt="members page entrance"
-          style={{ width: "55%" }}
+          style={{ width: "25%" }}
         />
       </a>
       <br />
       <a href="https://aqueous-taiga-71567.herokuapp.com/">
-        <img src="./image/brrger.png" alt="burger" style={{ width: "55%" }} />
+        <img src="./image/brrger.png" alt="burger" style={{ width: "25%" }} />
       </a>
       <br /> 
       <br />
@@ -140,7 +145,7 @@ function Portfolio() {
         alt="coding badge"
         style={{ width: "15%" }}
       />
-    
+    </div>
     </div>
   );
 }
