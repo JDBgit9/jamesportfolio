ebebfd54e457b55174b406f56b04d1768bcd38e1
JDBgit9 <jamesthejust99@outlook.com>
2022-06-21 00:10:52 -0400

    created new colums toplace item  across screen

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,148 +4,162 @@ import "./Portfolio.css";
 function Portfolio() {
   const dir = "./image";
   return (
-    <div className="row"> 
-    <div className="col-md-6">
-      <h2 className="port-head">My Portfolio</h2>
-      <br />
+    <div className="row">
+      <div className="col-md-4 one-col">
+        <h2 className="port-head">My Portfolio</h2>
+        <br />
+
+        <a href="https://jdbgit9.github.io/Tin-Dogger/">
+          <img
+            src="./image/tindog.png"
+            alt="tinder for dog app"
+            style={{ width: "90%" }}
+          />
+        </a>
+        <br />
+        <br />
+
+        <a href="https://pic-search.herokuapp.com/">
+          <img src="./image/cats.png" alt="cats" style={{ width: "85%" }} />
+        </a>
+
+        <p></p>
+        <a href="https://jdbgit9.github.io/Weather-App/">
+          <img
+            src="./image/weatherapp2.png"
+            alt="weather API page"
+            style={{ width: "75%" }}
+          />
+        </a>
+        <br />
+        <br />
     
-      <a href="https://jdbgit9.github.io/Tin-Dogger/">
-        <img
-          src="./image/tindog.png"
-          alt="tinder for dog app"
-          style={{ width: "90%" }}
-        />
-      </a>
-      <br />
-      <br />
+        <a href="https://6278331b098bd11537dbb012--countdown-timer-solutions.netlify.app/">
+          <img
+            src="./image/countdown.png"
+            alt="countdown API page"
+            style={{ width: "85%" }}
+          />
+        </a>
+        <br />
+        <br />
+        </div>
 
-      <a href="https://pic-search.herokuapp.com/">
-        <img src="./image/cats.png" alt="cats" style={{ width: "55%" }} />
-      </a>
 
-      <p></p>
-      <a href="https://jdbgit9.github.io/Weather-App/">
-        <img
-          src="./image/weatherapp2.png"
-          alt="weather API page"
-          style={{ width: "65%" }}
-        />
-      </a>
-      <br />
-      <br />
-  
-    </div>
-      
-    <div className="col-md-6 two-col">
+        <div className="col-md-4 two-col">
+        <a href="https://jdbgit9.github.io/Password-Generator/">
+          <img
+            src="./image/passwordgen.png"
+            alt="password generator"
+            style={{ width: "85%" }}
+          />
+        </a>
+        <br />
+        <br />
+        <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
+          <img
+            src="./image/codequiz.png"
+            alt="codequiz"
+            style={{ width: "65%" }}
+          />
+        </a>
+        <br />
+        <br />
+        <a href="https://notes-9.herokuapp.com/">
+          <img
+            src="./image/notetaker.png"
+            alt="note taker"
+            style={{ width: "65%" }}
+          />
+        </a>
+        <br />
+        <br />   
+          <a href="https://work-0ut.herokuapp.com/">
+            <img
+              src="./image/fitnesstracker.png"
+              alt="fitnesstracker"
+              style={{ width: "75%" }}
+            />
+          </a>
+          <br />
+          <br />
+</div>
+
+
+ <div className="col-md-4">
+          <a href="https://shimmering-sopapillas-3df067.netlify.app/">
+            <img
+              src="./image/shortly.png"
+              alt="landing page"
+              style={{ width: "65%" }}
+            />
+          </a>
+          <br />
+          <br />
+
+          <a href="https://jdbgit9.github.io/StreamGrub/">
+            <img
+              src="./image/moviedine2.png"
+              alt="StreamGrug"
+              style={{ width: "65%" }}
+            />
+          </a>
+          <br />
+          <br />
        
-      <a href="https://6278331b098bd11537dbb012--countdown-timer-solutions.netlify.app/">
-        <img
-          src="./image/countdown.png"
-          alt="countdown API page"
-          style={{ width: "65%" }}
-        />
-      </a>
-      <br />
-      <br />
+          <a href="https://theartroom.herokuapp.com/">
+            <img
+              src="./image/memberspage.png"
+              alt="members page entrance"
+              style={{ width: "65%" }}
+            />
+          </a>
+          <br />
+          <a href="https://aqueous-taiga-71567.herokuapp.com/">
+            <img
+              src="./image/brrger.png"
+              alt="burger"
+              style={{ width: "65%" }}
+            />
+          </a>
+          <br />
+          <br />
+<div />
 
-      <a href="https://jdbgit9.github.io/Password-Generator/">
-        <img
-          src="./image/passwordgen.png"
-          alt="password generator"
-          style={{ width: "85%" }}
-        />
-      </a>
-     <br />
-     <br />
-      <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
-        <img
-          src="./image/codequiz.png"
-          alt="codequiz"
-          style={{ width: "65%" }}
-        />
-      </a>
-      <br />
-      <br />
-      <a href="https://notes-9.herokuapp.com/">
-        <img
-          src="./image/notetaker.png"
-          alt="note taker"
-          style={{ width: "65%" }}
-        />
-      </a>
-      <br />
-      <br />
+<div className="col-md-8">
+          <a href="https://memebate.herokuapp.com/">
+            <img
+              src="./image/memebate.png"
+              alt="MemeBate"
+              style={{ width: "65%" }}
+            />
+          </a>
+          <br />
 
-      <div className="col-md-6">
-
-      <a href="https://work-0ut.herokuapp.com/">
-        <img
-          src="./image/fitnesstracker.png"
-          alt="fitnesstracker"
-          style={{ width: "75%" }}
-        />
-      </a>
-      <br />
-      <br />
-
-      <a href="https://shimmering-sopapillas-3df067.netlify.app/">
-        <img src="./image/shortly.png" alt="landing page" style={{ width: "25%" }} />
-      </a>
-      <br />
-      <br />
-
-      <a href="https://jdbgit9.github.io/StreamGrub/">
-        <img
-          src="./image/moviedine2.png"
-          alt="StreamGrug"
-          style={{ width: "25%" }}
-        />
-      </a>
-      <br />
-      <br />
-    </div>
-      <a href="https://theartroom.herokuapp.com/">
-        <img
-          src="./image/memberspage.png"
-          alt="members page entrance"
-          style={{ width: "25%" }}
-        />
-      </a>
-      <br />
-      <a href="https://aqueous-taiga-71567.herokuapp.com/">
-        <img src="./image/brrger.png" alt="burger" style={{ width: "25%" }} />
-      </a>
-      <br /> 
-      <br />
-    
-      <a href="https://memebate.herokuapp.com/">
-        <img
-          src="./image/memebate.png"
-          alt="MemeBate"
-          style={{ width: "25%" }}
-        />
-      </a>
-      <br />
-    
-      <br />
-      <img src="./image/nerdlook.png" alt="headshot" style={{ width: "45%" }} />
-      <a href="https://github.com/JDBgit9">
-        <br />
-        <br />
+          <br />
+          <img
+            src="./image/nerdlook.png"
+            alt="headshot"
+            style={{ width: "65%" }}
+          />
+          <a href="https://github.com/JDBgit9"></a>
+          <br />
+          <br />
+        </div>
         <img
           src="./image/github logo1.jpeg"
           alt="Github"
           style={{ width: "10%" }}
-        />         
-      </a>
-      <br />
-      <br/>
-      <img
-        src="./image/digitalbadge.png"
-        alt="coding badge"
-        style={{ width: "15%" }}
-      />
-    </div>
+        />
+
+        <br />
+        <br />
+        <img
+          src="./image/digitalbadge.png"
+          alt="coding badge"
+          style={{ width: "15%" }}
+        />
+      </div>
     </div>
   );
 }
