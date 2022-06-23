698c30b42413b22d953a723c7ae6bf92f07fe073
JDBgit9 <jamesthejust99@outlook.com>
2022-06-23 12:42:19 -0400

    rewrote passworden image to fix render in view

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -52,101 +52,100 @@ function Portfolio() {
           </a>
           <br />
           <br />
-      
+
           <a href="https://pic-search.herokuapp.com/">
             <img src="./image/cats.png" alt="cats" style={{ width: "80%" }} />
           </a>
           <br />
           <br />
-       
+
           <a href="https://jdbgit9.github.io/Password-Generator/">
-              <img
-                src="./image/passwordgen.png"
-                alt="password generator"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
-            <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
-              <img
-                src="./image/codequiz.png"
-                alt="codequiz"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
-            <a href="https://6278331b098bd11537dbb012--countdown-timer-solutions.netlify.app/">
-              <img
-                src="./image/countdown.png"
-                alt="countdown API page"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
+            <img
+              src="./image/passwordgen.png"
+              alt="password generator"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
+          <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
+            <img
+              src="./image/codequiz.png"
+              alt="codequiz"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
+          <a href="https://6278331b098bd11537dbb012--countdown-timer-solutions.netlify.app/">
+            <img
+              src="./image/countdown.png"
+              alt="countdown API page"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
 
-            <a href="https://work-0ut.herokuapp.com/">
-              <img
-                src="./image/fitnesstracker.png"
-                alt="fitnesstracker"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
-     
-            <a href="https://shimmering-sopapillas-3df067.netlify.app/">
-              <img
-                src="./image/shortly.png"
-                alt="landing page"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
+          <a href="https://work-0ut.herokuapp.com/">
+            <img
+              src="./image/fitnesstracker.png"
+              alt="fitnesstracker"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
 
-            <a href="https://jdbgit9.github.io/StreamGrub/">
-              <img
-                src="./image/moviedine2.png"
-                alt="StreamGrug"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
+          <a href="https://shimmering-sopapillas-3df067.netlify.app/">
+            <img
+              src="./image/shortly.png"
+              alt="landing page"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
 
-            <a href="https://aqueous-taiga-71567.herokuapp.com/">
-              <img
-                src="./image/brrger.png"
-                alt="burger"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
+          <a href="https://jdbgit9.github.io/StreamGrub/">
+            <img
+              src="./image/moviedine2.png"
+              alt="StreamGrug"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
 
-            <a href="https://theartroom.herokuapp.com/">
-              <img
-                src="./image/memberspage.png"
-                alt="members page entrance"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
-            <a href="https://memebate.herokuapp.com/">
-              <img
-                className="btm-pic"
-                src="./image/memebate.png"
-                alt="MemeBate"
-                style={{ width: "80%" }}
-              />
-            </a>
-            <br />
-            <br />
-       
+          <a href="https://aqueous-taiga-71567.herokuapp.com/">
+            <img
+              src="./image/brrger.png"
+              alt="burger"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
+
+          <a href="https://theartroom.herokuapp.com/">
+            <img
+              src="./image/memberspage.png"
+              alt="members page entrance"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
+          <a href="https://memebate.herokuapp.com/">
+            <img
+              className="btm-pic"
+              src="./image/memebate.png"
+              alt="MemeBate"
+              style={{ width: "80%" }}
+            />
+          </a>
+          <br />
+          <br />
         </div>
       </div>
     </div>
