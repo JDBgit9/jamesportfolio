ef961896f6870d0fcdee5777b2617ee080a38dec
JDBgit9 <jamesthejust99@outlook.com>
2022-07-01 16:05:40 -0400

    fixed home body and about body

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,14 @@ import "./Home.css";
 
 function Home() {
   return (
-    <div className="about-body">
+    <div className="home-body">
       <link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet" />
 <p id='head1' className='header1'>Welcome to JustisCode</p>
 <p id='head2' className='header1'>creative custom Web designs</p>
 <p id='head3' className='header1'>simple and awesome all the time</p>
 <p id='head4' className='header1'>Just...</p>
 <p id='head5' className='header1'>For you.</p>
-<button><a className="nav-link" href="/about">Continue</a></button>
+<button><a className="nav-link1" href="/about">Continue</a></button>
   <div className='light x1'></div>
   <div className='light x2'></div>
   <div className='light x3'></div>
