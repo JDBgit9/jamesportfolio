8a1d247d56032d69c696be0af5b9914a112192e9
JDBgit9 <jamesthejust99@outlook.com>
2022-06-30 23:15:38 -0400

    Changed syntax in the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,11 @@ function Home() {
   return (
     <div className="about-body">
       <link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet" />
-<p id='head1' className='header1'>Awesome designs</p>
-<p id='head2' className='header1'>Just</p>
-<p id='head3' className='header1'>For you</p>
-<p id='head4' className='header1'>simple and awesome all the time</p>
-<p id='head5' className='header1'>Welcome to JustisCode designs</p>
+<p id='head1' className='header1'>Welcome to JustisCode</p>
+<p id='head2' className='header1'>creative custom Web designs</p>
+<p id='head3' className='header1'>simple and awesome all the time</p>
+<p id='head4' className='header1'>Just...</p>
+<p id='head5' className='header1'>For you.</p>
 <button><a className="nav-link" href="/about">Continue</a></button>
   <div className='light x1'></div>
   <div className='light x2'></div>
