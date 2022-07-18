858b46763cf9bc3d07f0e1ada8c64c24e68ac3a3
JDBgit9 <jamesthejust99@outlook.com>
2022-07-18 10:54:12 -0400

    fixed ptag margins

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,28 +3,31 @@ import "./Portfolio.css";
 
 function Portfolio() {
   return (
-    <div className="container-fluid">
-      <div className="row">
+  <div className="container-fluid">
+    <div className="row">
         <div className="col-lg-4 clm-one">
+    
           <a href="https://jamesjustis.com">
             <img
               src="./image/jjdotcom.png"
               alt="responsive html/css bootstrap website"
-              style={{ width: "100%" }}
-            />
-          </a>
+              style={{ width: "100%" }}   />
+           </a>
           <br />
           <br />
+      
+        
           <a href="https://jdbgit9.github.io/Tin-Dogger/">
             <img
               src="./image/tindog.png"
               alt="tinder for dog app"
               style={{ width: "100%" }}
-            />
+            /> 
           </a>
           <br />
           <br />
-
+     
+      
           <a href="https://jdbgit9.github.io/Weather-App/">
             <img
               src="./image/weatherapp2.png"
@@ -34,7 +37,8 @@ function Portfolio() {
           </a>
           <br />
           <br />
-         
+     
+     
           <a href="https://notes-9.herokuapp.com/">
             <img
               src="./image/notetaker.png"
@@ -44,7 +48,7 @@ function Portfolio() {
           </a>
           <br />
           <br />
-       
+    
           <a href="https://todo-app-one-beta.vercel.app/">
             <img
               src="./image/todo.png"
@@ -54,14 +58,15 @@ function Portfolio() {
           </a>
           <br />
           <br />
-          </div>
+       </div>
        <div className="col-lg-4 clm-two">
+     
           <a href="https://pic-search.herokuapp.com/">
             <img src="./image/cats.png" alt="cats" style={{ width: "100%" }} />
           </a>
           <br />
           <br />
-
+    
           <a href="https://jdbgit9.github.io/Password-Generator/">
             <img
               src="./image/passwordgen.png"
@@ -71,90 +76,44 @@ function Portfolio() {
           </a>
           <br />
           <br />
+    
           <a href="https://jdbgit9.github.io/Code-Quiz/index.html">
-            <img
-              src="./image/codequiz.png"
-              alt="codequiz"
-              style={{ width: "100%" }}
-            />
-          </a>
-          <br />
-          <br />
-   
-          <a href="https://6278331b098bd11537dbb012--countdown-timer-solutions.netlify.app/">
-            <img
-              src="./image/countdown.png"
-              alt="countdown API page"
-              style={{ width: "100%" }}
-            />
-          </a>
-          <br />
-          <br />
-          </div>
-       <div className="col-lg-4 clm-three">
-          <a href="https://work-0ut.herokuapp.com/">
-            <img
-              src="./image/fitnesstracker.png"
-              alt="fitnesstracker"
+            <img src="./image/codequiz.png" alt="codequiz"
               style={{ width: "100%" }}
             />
           </a>
           <br />
           <br />
 
-          <a href="https://shimmering-sopapillas-3df067.netlify.app/">
-            <img
-              src="./image/shortly.png"
-              alt="landing page"
-              style={{ width: "100%" }}
-            />
-          </a>
-          <br />
-          <br />
-
-          <a href="https://jdbgit9.github.io/StreamGrub/">
-            <img
-              src="./image/moviedine2.png"
-              alt="StreamGrug"
-              style={{ width: "100%" }}
-            />
-          </a>
-          <br />
-          <br />
-     
+               <a href="https://6278331b098bd11537dbb012--countdown-timer-solutions.netlify.app/">
+               <img src="./image/countdown.png" alt="countdown API page" style={{ width: "100%" }}  /></a><br /><br />
+           
+       </div>
+        <div className="col-lg-4 clm-three">
+       
+               <a href="https://work-0ut.herokuapp.com/">
+               <img src="./image/fitnesstracker.png" alt="fitnesstracker" style={{ width: "100%" }} /></a><br /><br />
+      
+              <a href="https://shimmering-sopapillas-3df067.netlify.app/">
+              <img src="./image/shortly.png" alt="landing page" style={{ width: "100%" }} /></a><br /><br />
         
-          <a href="https://aqueous-taiga-71567.herokuapp.com/">
-            <img
-              src="./image/brrger.png"
-              alt="burger"
-              style={{ width: "100%" }}
-            />
-          </a>
-          <br />
-          <br />
-
-          <a href="https://theartroom.herokuapp.com/">
-            <img
-              src="./image/memberspage.png"
-              alt="members page entrance"
-              style={{ width: "100%" }}
-            />
-          </a>
-          <br />
-          <br />
-          <a href="https://memebate.herokuapp.com/">
-            <img
-              className="btm-pic"
-              src="./image/memebate.png"
-              alt="MemeBate"
-              style={{ width: "100%" }}
-            />
-          </a>
-          <br />
-          <br />
+                <a href="https://jdbgit9.github.io/StreamGrub/">
+                <img src="./image/moviedine2.png" alt="StreamGrug" style={{ width: "100%" }} /></a><br /><br /> 
+             
+                    <a href="https://aqueous-taiga-71567.herokuapp.com/">
+                   <img src="./image/brrger.png" alt="burger" style={{ width: "100%" }} /></a><br /><br />
+            
+                   <a href="https://theartroom.herokuapp.com/">
+                   <img src="./image/memberspage.png" alt="members page entrance" style={{ width: "100%" }} /></a><br /><br />
+              
+                      <a href="https://memebate.herokuapp.com/">
+                      <img className="btm-pic" src="./image/memebate.png" alt="MemeBate" style={{ width: "100%" }} /></a><br /><br />
+             
         </div>
-      </div>
+
     </div>
+  </div>
+
   );
 }
 
